33ee7a5f704370b7de613f794f2d1c3724e43e1f
Marko Trickovic <contact@markotrickovic.com>
2024-05-06 20:00:28 +0200

    feat(admin): customize the admin change list

--- polls/admin.py
+++ polls/admin.py
@@ -9,6 +9,9 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]})
